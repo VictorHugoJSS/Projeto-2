56bbe2ba3f59ca85c396dbac8d4765e271bbb04f
VictorHugoJSS <victor.-hugo@outlook.com.br>
2024-04-24 21:34:31 -0300

    .

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 public class Biblioteca{
-    private Scanner scanner = new Scanner(System.in);
     ArrayList <Cliente> clientes = new ArrayList<>();
     ArrayList <Funcionario> proletariado = new ArrayList<>();
     ArrayList <Livro> livros = new ArrayList<>();
@@ -28,35 +27,41 @@ public class Biblioteca{
     void add_livro(Scanner scanner){
         Livro test = new Livro();
         String titulo, autor, id;
-        int add;
-
-        while(true){
-            System.out.print("Digite o Titulo do livro: ");
-            titulo = scanner.nextLine();
-            System.out.print("Digite o Autor do livro: ");
-            autor = scanner.nextLine();
-            System.out.print("Digite o Id do livro: ");
-            id = scanner.nextLine();
-            test.setInfo(titulo, autor, id);
-            livros.add(test);
-            System.out.print("Deseja adicionar um novo livro? (Digite 1 para adicionar ou 0 para sair)");
-            add = scanner.nextInt();
-            while (add != 1 || add != 0){
-                System.out.print("Deseja adicionar um novo livro? (Digite 1 para adicionar ou 0 para sair)");
-                add = scanner.nextInt();
-            }
-            if (add == 0){
-                break;
-            }
-        }
+        System.out.println("Digite o Titulo do livro: ");
+        titulo = scanner.nextLine();
+        System.out.println("Digite o Autor do livro: ");
+        autor = scanner.nextLine();
+        System.out.println("Digite o Id do livro: ");
+        id = scanner.nextLine();
+        test.setInfo(titulo, autor, id);
+        livros.add(test);
     }
 
-    void remove_livro(Scanner scanner){
-        int j = 1;
+    void remover_livro(Scanner scanner){
+        int r, conf;
         while (true){
+            int j = 1;
+            System.out.println("-----------------------------------------------");
             System.out.println("Id | Titulo | Autor | Status");
             for(Livro i : livros){
-                System.out.println(j++ + " " + i.getId() + " " + i.getTitulo() + " " + i.getAutor() + i.getStatus());
+                System.out.println(j++ + " |" + i.getId() + " |" + i.getTitulo() + " |" + i.getAutor() + " |"+ i.getStatus());
+            }
+            System.out.println("-----------------------------------------------");
+            System.out.print("Digite o numero do livro que você quer remover: ");
+            r = scanner.nextInt();
+            while (r < 0 || r > j){
+                System.out.print("Digite o numero do livro que você quer remover: ");
+                r = scanner.nextInt();
+            }
+            livros.remove(r-1);
+            System.out.println("Deseja remover mais um livro? (Digite 1 para remover um novo livro ou 0 para sair)");
+            conf = scanner.nextInt();
+            while (conf != 1 && conf != 0){
+                System.out.println("Deseja remover mais um livro? (Digite 1 para remover um novo livro ou 0 para sair)");
+                conf = scanner.nextInt();
+            }
+            if (conf == 0){
+                break;
             }
         }
     }
--- Main.java
+++ Main.java
@@ -2,10 +2,15 @@
    Projeto: Sistema de Gerenciamento de Biblioteca
    Aluno: Victor Hugo José Sales da Silva
    Data da ultima Atualização: 14/04/24 - 18:59*/ 
-
+import java.util.*;
 public class Main{
 	
 	public static void main (String[] args){
-      
+      Scanner scanner = new Scanner(System.in);
+      Biblioteca teste = new Biblioteca();
+
+      teste.add_livro(scanner);
+      teste.add_livro(scanner);
+      teste.remover_livro(scanner);
 	}
 }

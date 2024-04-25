90aaa7ca39bd6cfa945c5f7ac493ed68fac47b2b
VictorHugoJSS <victor.-hugo@outlook.com.br>
2024-04-24 21:46:42 -0300

    .

--- Main.java
+++ Main.java
@@ -4,13 +4,9 @@
    Data da ultima Atualização: 14/04/24 - 18:59*/ 
 import java.util.*;
 public class Main{
-	
-	public static void main (String[] args){
-      Scanner scanner = new Scanner(System.in);
-      Biblioteca teste = new Biblioteca();
-
-      teste.add_livro(scanner);
-      teste.add_livro(scanner);
-      teste.remover_livro(scanner);
-	}
+      public static void main (String[] args){
+            Scanner scanner = new Scanner(System.in);
+            Biblioteca teste = new Biblioteca();
+            
+      }
 }

ab58c7c448bd27f5467df3f4bcd0cd7074842c25
VictorHugoJSS <victor.-hugo@outlook.com.br>
2024-04-24 21:54:20 -0300

    .

--- Main.java
+++ Main.java
@@ -2,11 +2,7 @@
    Projeto: Sistema de Gerenciamento de Biblioteca
    Aluno: Victor Hugo José Sales da Silva
    Data da ultima Atualização: 14/04/24 - 18:59*/ 
-import java.util.*;
 public class Main{
       public static void main (String[] args){
-            Scanner scanner = new Scanner(System.in);
-            Biblioteca teste = new Biblioteca();
-            
       }
 }

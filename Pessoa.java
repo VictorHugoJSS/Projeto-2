9cd0de001e9607d8b5776056034f41d063f32dad
nodemi-alt <163960947+nodemi-alt@users.noreply.github.com>
2024-04-27 22:33:10 -0300

    Parametros para login adicionados
    mais metodos de comparação e busca serão adicionados futuramente

--- Pessoa.java
+++ Pessoa.java
@@ -1,7 +1,31 @@
 public class Pessoa{
-	String nome;
-	String endereco;
-	String telefone;
+	private String nome;
+	private String endereco;
+	private String telefone;
+	private String ID;
+    private String Senha;
+
+	void setId (String id){
+        this.ID = id;
+    }
+
+    String getId(){
+        return ID;
+    }
+	
+    void setSenha(String Senha){
+        this.Senha = Senha;
+    }
+
+    String getSenha(){
+        return Senha;
+    }
+
+    int verificarSenha(String Senha)
+    {
+        if ( this.Senha.equals(Senha) ) { return 1; }
+        else { return 0; }
+    }
 
 	void SetInfo(String nome, String end, String tel){
 		this.nome = nome;
@@ -20,9 +44,11 @@ public class Pessoa{
 	String GetTelefone(){
 		return telefone;
 	}
+
 	void get_info(){
 		System.out.println("Nome: " + nome);
 		System.out.println("Endereço: " + endereco);
 		System.out.println("Telefone: " + telefone);
 	}
-}
+} 
+    

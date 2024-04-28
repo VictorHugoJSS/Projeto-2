528fef65908cd4f431e23783d4f912b021934bb6
VictorHugoJSS <victor.-hugo@outlook.com.br>
2024-04-28 10:29:41 -0300

    remove: main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-/* Cadeira: Programação Orientada a objeto
-   Projeto: Sistema de Gerenciamento de Biblioteca
-   Aluno: Victor Hugo José Sales da Silva
-   Data da ultima Atualização: 14/04/24 - 18:59*/ 
-public class Main{
-      public static void main (String[] args){
-      }
-}

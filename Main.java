a68d305a9b0e05740096a829d5f61c73d0c9118b
nodemi-alt <163960947+nodemi-alt@users.noreply.github.com>
2024-04-30 06:05:55 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main (String[] args)
     {
         Biblioteca stret = new Biblioteca();
-        stret.menu();
+        stret.menu(); // O inicio / tela de login do programa
     }
 }
